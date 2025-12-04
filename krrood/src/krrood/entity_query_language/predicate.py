9e27dded44b6ffca16a15757d5a666e173c6cab7
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-12-04 21:25:36 +0100

    [EQL] fix symbolic_function.

--- krrood/src/krrood/entity_query_language/predicate.py
+++ krrood/src/krrood/entity_query_language/predicate.py
@@ -157,7 +157,7 @@ def get_function_argument_names(function: Callable) -> List[str]:
 
 
 def merge_args_and_kwargs(
-    function: Callable, args, kwargs, ignore_first: bool = True
+    function: Callable, args, kwargs, ignore_first: bool = False
 ) -> Dict[str, Any]:
     """
     Merge the arguments and keyword-arguments of a function into a dict of keyword-arguments.
