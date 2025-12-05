7ce6d478efdc63c0210d44bead6d15f66d820810
Jonas Dech <j.dech@gmx.net>
2025-12-05 16:27:01 +0100

    First idea alternative mapping

--- pycram/src/pycram/alternative_motion_mappings/hsrb_motion_mapping.py
+++ pycram/src/pycram/alternative_motion_mappings/hsrb_motion_mapping.py
@@ -1,12 +1,19 @@
 from dataclasses import dataclass
 
+from typing_extensions import ClassVar
+
+from krrood.entity_query_language.symbolic import Literal
 from semantic_digital_twin.robots.abstract_robot import AbstractRobot
+from semantic_digital_twin.robots.hsrb import HSRB
+from ..datastructures.enums import ExecutionType
+from ..robot_plans import MoveMotion
 
 from ..robot_plans.motions.base import AlternativeMotionMapping
 
 
-class HSRBTCPMotionMapping(AlternativeMotionMapping):
+class HSRBMoveMotionMapping(MoveMotion, AlternativeMotionMapping[HSRB]):
 
-    @property
-    def motion_chart(self):
-        return self.robot_view.get_motion_chart("hsrb_tcp_motion")
+    execution_type = ExecutionType.REAL
+
+    def perform(self):
+        pass
