4b94e5ae44a774237da754ffa26ce5e87b24b33f
jonas Dech <j.dech@gmx.net>
2026-01-10 11:51:47 +0100

    Improved PosePublisher

--- semantic_digital_twin/src/semantic_digital_twin/adapters/ros/pose_publisher.py
+++ semantic_digital_twin/src/semantic_digital_twin/adapters/ros/pose_publisher.py
@@ -1,9 +1,11 @@
 import time
 from dataclasses import dataclass, field
+from uuid import UUID, uuid4
 
+from builtin_interfaces.msg import Duration
 import numpy as np
 import rclpy
-from std_msgs.msg import ColorRGBA
+from std_msgs.msg import ColorRGBA, Header
 from typing_extensions import List, Any, Dict
 from visualization_msgs.msg import MarkerArray, Marker
 
@@ -29,18 +31,35 @@ class PosePublisher(StateChangeCallback):
     """
     ROS node handle, used to create the publisher.
     """
+    lifetime: int = 0
+    """
+    Lifetime of the PosePublisher and viz marker in seconds. If the lifetime is 0 the marker will stay indefinitely.
+    """
+    text: str = None
+    """
+    Text to display at the pose position 
+    """
     topic_name: str = "/semworld/viz_marker"
     """
     Topic name to publish the pose marker on.
     """
-    publish_chain: bool = False
-    """
-    If true publishes all transforms between map and pose
-    """
 
     publisher: Any = field(init=False)
+    """
+    Ros publisher for viz marker
+    """
+    _ns: UUID = field(init=False, default_factory=uuid4)
+    """
+    Unique id to differentiate the different pose marker
+    """
+    end_time: float = field(init=False)
+    """
+    End time for this PosePublisher, used for lifetime only if given lifetime is greater than 0
+    """
 
     def _notify(self):
+        if self.lifetime > 0 and time.time() >= self.end_time:
+            self.pause()
         world = self.pose.reference_frame._world
         global_pose = world.transform(self.pose, self.world.root)
         marker_array = self._create_marker_array(global_pose)
@@ -49,6 +68,7 @@ class PosePublisher(StateChangeCallback):
     def __post_init__(self):
         self.publisher = self.node.create_publisher(MarkerArray, self.topic_name, 10)
         time.sleep(0.2)
+        self.end_time = time.time() + self.lifetime
         self._notify()
 
     def _create_marker_array(
@@ -60,20 +80,21 @@ class PosePublisher(StateChangeCallback):
         :param global_pose: The pose to in global frame
         """
         marker_array = MarkerArray()
+        position = global_pose.to_position().to_np()[:3]
+        orientation = global_pose.to_rotation_matrix().to_quaternion().to_np()
+
+        p = Pose(
+            position=Point(**dict(zip(["x", "y", "z"], position.tolist()))),
+            orientation=Quaternion(
+                **dict(zip(["x", "y", "z", "w"], orientation.tolist()))
+            ),
+        )
         for i in range(3):
             axis = [0.0, 0.0, 0.0]
-            axis[i] = 1.0  # Defines the length of the arrow
+            axis[i] = 0.5  # Defines the length of the arrow
             color = [0.0, 0.0, 0.0, 1.0]
             color[i] = 1.0
-            position = global_pose.to_position().to_np()[:3]
-            orientation = global_pose.to_rotation_matrix().to_quaternion().to_np()
 
-            p = Pose(
-                position=Point(**dict(zip(["x", "y", "z"], position.tolist()))),
-                orientation=Quaternion(
-                    **dict(zip(["x", "y", "z", "w"], orientation.tolist()))
-                ),
-            )
             c = ColorRGBA(**dict(zip(["r", "g", "b", "a"], color)))
 
             end_point = Point(
@@ -87,6 +108,27 @@ class PosePublisher(StateChangeCallback):
                     p,
                     Point(**dict(zip(["x", "y", "z"], position.tolist()))),
                     end_point,
+                )
+            )
+        if self.text:
+            marker_array.markers.append(
+                Marker(
+                    action=Marker.ADD,
+                    type=Marker.TEXT_VIEW_FACING,
+                    text=self.text,
+                    ns=str(self._ns),
+                    id=4,
+                    pose=p,
+                    scale=Vector3(z=0.1),
+                    lifetime=Duration(
+                        sec=(
+                            round(self.end_time - time.time())
+                            if self.lifetime > 0
+                            else 0
+                        )
+                    ),
+                    color=ColorRGBA(r=1.0, g=1.0, b=1.0, a=1.0),
+                    header=Header(frame_id="map"),
                 )
             )
         return marker_array
@@ -113,11 +155,13 @@ class PosePublisher(StateChangeCallback):
         m.id = _id
         m.header.frame_id = "map"
         m.pose = pose
-
+        m.lifetime = Duration(
+            sec=round(self.end_time - time.time()) if self.lifetime > 0 else 0
+        )
         m.points = [start_point, end_point]
 
-        m.scale = Vector3(x=0.05, y=0.1, z=0.2)
+        m.scale = Vector3(x=0.025, y=0.05, z=0.1)
         m.color = color
-        m.ns = self.pose.reference_frame.name.name
+        m.ns = str(self._ns)
 
         return m
