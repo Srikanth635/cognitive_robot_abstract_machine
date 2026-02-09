b867fcc23fe9fa16c62373b860a772d8b0ba1c01
LucaKro <luc_kro@uni-bremen.de>
2026-02-09 01:20:29 +0100

    handle.root instead of handle.body

--- pycram/src/pycram/robot_plans/actions/composite/transporting.py
+++ pycram/src/pycram/robot_plans/actions/composite/transporting.py
@@ -84,7 +84,7 @@ class TransportAction(ActionDescription):
                 if sem_anno:
                     SequentialPlan(
                         self.context,
-                        OpenActionDescription(sem_anno[0].handle.body, self.arm),
+                        OpenActionDescription(sem_anno[0].handle.root, self.arm),
                     ).perform()
         SequentialPlan(self.context, ParkArmsActionDescription(Arms.BOTH)).perform()
         pickup_loc = CostmapLocation(
