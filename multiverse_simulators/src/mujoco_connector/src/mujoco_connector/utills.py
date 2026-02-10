6624285f6f06eb2b7e76a6f37e50d6e252539fca
Giang Nguyen <hoanggia@uni-bremen.de>
2026-02-10 10:24:19 +0100

    Add docs

--- multiverse_simulators/src/mujoco_connector/src/mujoco_connector/utills.py
+++ multiverse_simulators/src/mujoco_connector/src/mujoco_connector/utills.py
@@ -2,6 +2,12 @@ import os
 import mujoco
 
 def get_multiverse_connector_plugins():
+    """
+    Get the list of MuJoCo plugin files for the current MuJoCo version.
+    The plugins are expected to be located in a directory named "mujoco_plugin/mujoco-{version}" relative to this file.
+
+    :return: A list of file paths to the MuJoCo plugin files.
+    """
     mujoco_version = str(mujoco.mj_version())
     possible_mujoco_plugins_dir = os.path.join(os.path.dirname(__file__), "..", "..", "mujoco_plugin", f"mujoco-{mujoco_version[0]}.{mujoco_version[1]}.{mujoco_version[2]}")
     file_ext = ".so" if os.name == "posix" else ".dll"
