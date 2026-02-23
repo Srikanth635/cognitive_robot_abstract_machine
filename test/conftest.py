434d7c143fa368e509bb4bc3db9400a26080e521
Simon Stelter <stelter@uni-bremen.de>
2026-02-23 12:50:53 +0100

    fixed giskard/semdt test

--- test/conftest.py
+++ test/conftest.py
@@ -666,7 +666,6 @@ def pr2_world_state_reset(pr2_world_setup):
     state = world.state.data.copy()
     yield world
     world.state.data[:] = state
-    world.notify_state_change()
 
 
 ###############################
