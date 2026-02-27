d6539db7229950b8db976fda8ac9b60866cfe2e2
jonas Dech <j.dech@gmx.net>
2026-02-27 12:56:13 +0100

    Plan Domain

--- pycram/src/pycram/parameter_rules/default_type_domains.py
+++ pycram/src/pycram/parameter_rules/default_type_domains.py
@@ -4,21 +4,22 @@ from itertools import product
 from pycram.datastructures.dataclasses import Context
 from pycram.datastructures.enums import ApproachDirection, VerticalAlignment, Arms
 from pycram.datastructures.grasp import GraspDescription
-from pycram.parameter_inference import Domain
+from pycram.parameter_inference import DomainSpecification
+from pycram.parameter_rules.default_rules import ArmsFitGraspDescriptionRule
 from pycram.plan import Plan
 from pycram.utils import get_all_values_in_enum
 from semantic_digital_twin.robots.abstract_robot import Manipulator
 
 
 @dataclass
-class EnumDomain(Domain):
+class EnumDomainSpecification(DomainSpecification):
 
     def domain(self, context: Context):
         return get_all_values_in_enum(self.domain_type)
 
 
 @dataclass
-class GraspDomain(Domain):
+class GraspDomainSpecification(DomainSpecification):
 
     manipulator: Manipulator
 
@@ -33,11 +34,16 @@ class GraspDomain(Domain):
 
 
 @dataclass
-class SemanticAnnotationDomain(Domain):
+class SemanticAnnotationDomainSpecification(DomainSpecification):
 
     def domain(self, context: Context):
         return context.world.get_semantic_annotations_by_type(self.domain_type)
 
 
 def load_default_domains(plan: Plan):
-    plan.parameter_infeerer.add_domains(EnumDomain(Arms))
+    grasp_domains = [
+        GraspDomainSpecification(GraspDescription, man)
+        for man in plan.context.world.get_semantic_annotations_by_type(Manipulator)
+    ]
+    plan.parameter_infeerer.add_domains(EnumDomainSpecification(Arms), *grasp_domains)
+    # plan.parameter_infeerer.add_rule(ArmsFitGraspDescriptionRule())
