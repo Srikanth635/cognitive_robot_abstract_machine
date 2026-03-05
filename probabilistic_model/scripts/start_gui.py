5643b9179eddb5d9a09758b221b1e4f7aa5b32fc
Tom Schierenbeck <tom_sch@uni-bremen.de>
2026-03-05 17:19:33 +0100

    [PM] Factory for GUI

--- probabilistic_model/scripts/start_gui.py
+++ probabilistic_model/scripts/start_gui.py
@@ -7,9 +7,7 @@ from probabilistic_model.probabilistic_circuit.rx.probabilistic_circuit import (
     ProbabilisticCircuit,
 )
 
-from probabilistic_model.gui.main_window import MainWindow
-from probabilistic_model.probabilistic_model import ProbabilisticModel
-from qt_material import apply_stylesheet
+from probabilistic_model.gui.factory import GUIFactory
 
 
 def main(model_path: str = None):
@@ -18,15 +16,6 @@ def main(model_path: str = None):
 
     :param model_path: The path to the model to display in the GUI.
     """
-    # Recommended for some Linux environments/Docker
-    os.environ["QTWEBENGINE_DISABLE_SANDBOX"] = "1"
-
-    # Required for QWebEngineView to work in many environments
-    QApplication.setAttribute(Qt.ApplicationAttribute.AA_ShareOpenGLContexts)
-
-    # Sometimes helps with rendering on certain GPUs/drivers
-    # In Qt6, AA_UseDesktopOpenGL is still available but often default.
-    # On some systems, AA_UseOpenGLES or AA_UseSoftwareOpenGL might be needed instead.
     model_path = "/home/tom_sch/.config/JetBrains/PyCharm2025.3/scratches/model.pm"
 
     if model_path is None:
@@ -35,11 +24,8 @@ def main(model_path: str = None):
         with open(model_path) as f:
             model = ProbabilisticCircuit.from_json(json.load(f))
 
-    app = QApplication(sys.argv)
-    apply_stylesheet(app, theme="dark_amber.xml")
-    window = MainWindow(model=model)
-    window.show()
-    sys.exit(app.exec())
+    factory = GUIFactory(model=model, theme="dark_amber.xml")
+    factory.start_gui()
 
 
 if __name__ == "__main__":
