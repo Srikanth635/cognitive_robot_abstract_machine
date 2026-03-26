38a654d5b2d89eb4adb06fdc47e57a24cff291dc
Tom Schierenbeck <tom_sch@uni-bremen.de>
2026-03-26 15:12:03 +0100

    [KRROOD] KRROOD tests are completely working,

--- test/krrood_test/test_ormatic/test_repr.py
+++ test/krrood_test/test_ormatic/test_repr.py
@@ -7,7 +7,7 @@ from ..dataset.ormatic_interface import PositionDAO, PoseDAO, Position4DDAO
 def test_repr_includes_scalar_columns_only():
     # Given a simple object with scalar columns only
     p = Position(1.0, 2.0, 3.0)
-    dao: PositionDAO = PositionDAO.to_dao(p)
+    dao: PositionDAO = to_dao(p)
 
     s = repr(dao)
 
@@ -18,7 +18,7 @@ def test_repr_does_not_recurse_into_relationships():
     o = Orientation(1.0, 2.0, 3.0, None)
     pose = Pose(p, o)
 
-    dao: PoseDAO = PoseDAO.to_dao(pose)
+    dao: PoseDAO = to_dao(pose)
 
     s = repr(dao)
 
@@ -26,7 +26,7 @@ def test_repr_does_not_recurse_into_relationships():
 def test_repr_works_for_inherited_columns():
     # Given an inherited DAO that adds an extra column
     p4d = Position4D(1.0, 2.0, 3.0, 4.0)
-    dao: Position4DDAO = Position4DDAO.to_dao(p4d)
+    dao: Position4DDAO = to_dao(p4d)
 
     s = repr(dao)
 
