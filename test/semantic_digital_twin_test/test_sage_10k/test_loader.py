12b31a1c5d3c1172a6a7c427daf0e6db9390f3a6
Tom Schierenbeck <tom_sch@uni-bremen.de>
2026-03-31 16:04:43 +0200

    [SemDT] Sage10k integration starts.

---
+++ test/semantic_digital_twin_test/test_sage_10k/test_loader.py
@@ -0,0 +1,14 @@
+from krrood.adapters.json_serializer import from_json
+from semantic_digital_twin.adapters.sage_10k_dataset.loader import Sage10kDatasetLoader
+from semantic_digital_twin.adapters.sage_10k_dataset.schema import Sage10kScene
+
+
+def test_loader():
+    loader = Sage10kDatasetLoader(
+        scene_url="https://huggingface.co/datasets/nvidia/SAGE-10k/resolve/main/scenes/20251213_020526_layout_84b703fb.zip",
+    )
+    target_path = loader._download_scene()
+    unzipped = loader._unzip_scene(target_path)
+    json_data = loader._parse_json(unzipped)
+    data = Sage10kScene.from_json(json_data)
+    print(data)
